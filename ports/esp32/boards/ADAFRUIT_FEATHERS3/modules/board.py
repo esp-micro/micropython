d53878f7c8809bbcbbb5cee759a3d243873cdbb2
Edwin Nel <nel.edwin@gmail.com>
2024-11-06 21:21:37 +0200

    Adafruit Feather ESP32S3 4MB Flash 2MB PSRAM

---
+++ ports/esp32/boards/ADAFRUIT_FEATHERS3/modules/board.py
@@ -0,0 +1,56 @@
+import feathers3
+
+BOARD_IDENTIFIER = "ADA_FEATHER_S3" # Adafruit Feather S3
+
+I2C_SCL = feathers3.I2C_SCL
+I2C_SDA = feathers3.I2C_SDA
+I2C_PWR = feathers3.I2C_PWR
+
+RGB_DATA = feathers3.RGB_DATA
+RGB_PWR = feathers3.RGB_PWR
+
+LED = feathers3.LED
+
+def get_bat_voltage():
+    """Read and return the battery voltage from the fuel gauge."""
+    return feathers3.get_bat_voltage()
+
+
+def get_state_of_charge():
+    """Read and return the state of charge from the fuel gauge."""
+    return feathers3.get_state_of_charge()
+
+
+def reset_battery_monitor():
+    """Reset the battery monitor to default values."""
+    feathers3.reset_battery_monitor()
+
+
+def quick_start_battery_monitor():
+    """Perform a quick start to reset the SOC calculation in the chip."""
+    feathers3.quick_start_battery_monitor()
+
+
+def set_pixel_power(state):
+    """Enable or Disable power to the onboard NeoPixel to either show colour, or to reduce power for deep sleep."""
+    feathers3.set_pixel_power(state)
+
+
+def rgb_color_wheel(wheel_pos):
+    """Color wheel to allow for cycling through the rainbow of RGB colors."""
+    return feathers3.rgb_color_wheel(wheel_pos)
+
+
+def led_set(state):
+    """Set the state of the BLUE LED on IO13"""
+    feathers3.led_set(state)
+
+
+def toggle_led(state):
+    """Toggle the BLUE LED on IO13"""
+    feathers3.toggle_led(state)
+
+
+def lowpower():
+    """Enter low power mode."""
+    feathers3.lowpower()
---
+++ ports/esp32/boards/ADAFRUIT_FEATHERS3/mpconfigvariant_FLASH_4M.cmake
@@ -0,0 +1,4 @@
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/ADAFRUIT_FEATHERS3/sdkconfig.flash_4m
+)
